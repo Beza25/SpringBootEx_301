9cd658bd5e540018791070858284bd2be75ac0ac
GBTC405016UR <GBTC405016UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-08-20 16:45:24 -0400

    springBootEx_301

---
+++ src/main/java/com/example/demo/Dog.java
@@ -0,0 +1,50 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class Dog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotNull
+    private String name;
+    @Min(1)
+    private int age;
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
+
+
+
+
+
+
